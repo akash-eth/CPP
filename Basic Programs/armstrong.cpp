c645b2176a7da3633d43a1a595172e30ac036489
Akash Bhardwaj <akkiworkspace@gmail.com>
2021-06-15 11:55:06 +0530

    pattern updates

---
+++ Basic Programs/armstrong.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int rev = 0;
+    while(n>0) {
+        int rem = n%10;
+        rev = rev*10 + rem;
+        n = n/10;
+    }
+    if(n == rev) {
+        cout  << "Armstrong";
+    }
+    else {
+        cout << "Not an armstrong number";
+    }
+
+    return 0;
+}
