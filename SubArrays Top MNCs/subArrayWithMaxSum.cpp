3484f910b49cf91aece1083ae5eccad1e5944e8f
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-08-06 00:15:44 +0530

    subarray with max sum

---
+++ SubArrays Top MNCs/subArrayWithMaxSum.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    int mx = INT_MIN;
+
+    for(int i=0; i<n; i++) {
+        for(int j=i; j<n; j++) {
+            int sum=0;
+            for(int k=i; k<=j; k++) {
+                sum += arr[k];
+            }
+            mx = max(mx, sum);
+        }
+    }
+
+    cout << mx;
+
+    return 0;
+}
