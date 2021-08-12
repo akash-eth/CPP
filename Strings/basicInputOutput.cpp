9b48d699466db25933ad8f7379be6bf77cef5ca1
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-08-12 12:34:34 +0530

    Started with strings

---
+++ Strings/basicInputOutput.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+
+    // to get space as well use getline fucntion !!
+    string str3;
+    getline(cin, str3);
+    cout << str3;
+
+    // string str;
+
+    // cin >> str;
+    // cout << str << endl;
+
+    // string str1(5, 'a');
+    // cout << str1 << endl;
+
+    // // no space in between chars !!
+    // string str2 = "AkashBhardwaj";
+    // cout << str2 << endl;
+
+    return 0;
+}
