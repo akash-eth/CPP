7abf8afe546e7533660c05eaa283ef479c5c3dbc
akashbhardwaj04 <akkiworkspace@gmail.com>
2021-08-17 12:47:11 +0530

    most occuring element

---
+++ String Challenges/mostOccuringElements.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+    string s = "aabbabskfbnfbfjdkcbcbk";
+    
+    char freq[26];
+    for(int i=0; i<26; i++) {
+        freq[i] = 0;
+    }
+
+    for(int i=0; i<s.length(); i++) {
+        freq[s[i] - 'a']++; 
+    }
+
+    char ans = 'a';
+    int maxF = 0;
+
+    for(int i=0; i<26; i++) {
+        if(freq[i] > maxF) {
+            maxF = freq[i];
+            ans = i+'a';
+        }
+    }
+
+    cout << maxF << endl;
+    cout << ans;
+
+    return 0;
+}
